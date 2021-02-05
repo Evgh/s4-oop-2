cb23678dbef55f421ad1be150368634fc990171a
Evgh <kasperovich.official@gmail.com>
2021-02-06 00:55:46 +0300

    Создала классы, с которыми работаем Разместила эу на форме

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -295,7 +295,7 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(430, 550);
+            this.dataGridView1.Size = new System.Drawing.Size(771, 550);
             this.dataGridView1.TabIndex = 16;
             // 
             // Form1
